b829fe896e6afb5603837c9efa971c86a7a8fe2c
Dhiraj Kumar <77327143+dhiru9599@users.noreply.github.com>
2023-10-04 16:26:14 +0530

    Create employee.cs

---
+++ employee.cs
@@ -0,0 +1,8 @@
+public virtual void Display()
+{
+    Console.WriteLine("Employee Details");
+    Console.WriteLine("Employee Id: {0}", Employeeld);
+    Console.WriteLine("Name: {0}", Name);
+    Console.WriteLine("Email: {0}", Email);
+    Console.WriteLine("Mobile Number: {0}", MobileNumber);
+}
